2d2ac247f35bce42d6dade78c9652ec973c81ea8
ryuhc <ryuhc@ozys.net>
2025-06-09 14:19:20 +0900

    fix: type definition for theme

--- src/context/StyleProvider.tsx
+++ src/context/StyleProvider.tsx
@@ -8,6 +8,13 @@ import type { SwapWidgetProps } from '@/types/index.d.ts'
 
 import { darkColors, lightColors } from '@/constants/colors'
 
+type ThemeType = typeof lightColors
+declare module '@emotion/react' {
+  export interface Theme {
+    colors: ThemeType
+  }
+}
+
 export default function StyleProvider({
   theme,
   children
